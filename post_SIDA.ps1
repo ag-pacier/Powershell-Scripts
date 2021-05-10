b70e91763a31fc2782a0125bba2260685cc192ea
ag-pacier <lead.diet@gmail.com>
2021-05-10 12:20:25 -0400

    Started Post install script

---
+++ post_SIDA.ps1
@@ -0,0 +1,17 @@
+<#	
+Created by Gage Pacier
+SIDA swap script helper
+#>
+
+
+Write-Host "Please put in root login!"
+
+$creds = Get-Credential
+
+Write-Host "Starting printer install, please use IP: "
+
+Start-Process -FilePath ".\HP_printer.exe" -Credential $creds
+
+$Printername = Read-Host -Prompt "Name of the printer you just installed"
+
+(Get-WMIObject -ClassName win32_printer |Where-Object -Property Name -eq "$Printername").SetDefaultPrinter()
