c8681cc8ecda3153692cdf66907ecddcdef29ca4
Gage Pacier <lead.diet@gmail.com>
2021-05-10 12:53:11 -0400

    Reoganized SIDA stuff

---
+++ SIDA/2_post_SIDA_root.ps1
@@ -0,0 +1,9 @@
+<#
+Gage Pacier's root SIDA script
+#>
+
+#Requires -RunAsAdministrator
+
+Write-Host "Starting printer install, please use IP: 10.11.66.253"
+
+Start-Process -FilePath ".\HP_printer.exe"
--- SIDA/3_post_SIDA_train.ps1
+++ SIDA/3_post_SIDA_train.ps1
@@ -3,12 +3,11 @@ Created by Gage Pacier
 SIDA swap script helper
 #>
 
-
 Write-Host "Please put in root login!"
 
 $creds = Get-Credential
 
-Write-Host "Starting printer install, please use IP: "
+Write-Host "Starting printer install, please use IP: 10.11.66.253"
 
 Start-Process -FilePath ".\HP_printer.exe" -Credential $creds
 
