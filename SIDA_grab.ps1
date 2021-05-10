2ee5d4af490ef80639c27f8d7522e46be3f360df
ag-pacier <lead.diet@gmail.com>
2021-05-10 11:46:27 -0400

    Create SIDA_grab.ps1

---
+++ SIDA_grab.ps1
@@ -0,0 +1,16 @@
+﻿<#	
+Created by Gage Pacier
+SIDA swap script helper
+#>
+
+$MAC = Get-CimInstance win32_networkadapterconfiguration | Where-Object -Property "Description" -Match "Realtek" | Select-Object -ExpandProperty macaddress
+$MAC = $MAC -replace ":","-"
+$Serial = Get-CimInstance win32_bios | Select-Object -ExpandProperty SerialNumber
+
+$OutPut = "{0}.txt" -f $env:COMPUTERNAME
+
+New-Item -Path $PWD -ItemType "file" -Name $OutPut
+
+$Contents = "MAC: {0},, Serial: {1}" -f $MAC, $Serial
+
+Add-Content -Path $OutPut -Value $Contents
