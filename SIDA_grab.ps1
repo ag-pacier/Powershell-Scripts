c6693debf1bb563501fb7ddc0d3fb0dff9fab7ec
ag-pacier <lead.diet@gmail.com>
2021-05-10 12:01:46 -0400

    Added message to help others

--- SIDA_grab.ps1
+++ SIDA_grab.ps1
@@ -14,3 +14,9 @@ New-Item -Path $PWD -ItemType "file" -Name $OutPut
 $Contents = "MAC: {0},, Serial: {1}" -f $MAC, $Serial
 
 Add-Content -Path $OutPut -Value $Contents
+
+$message = "Please input info in {0} into Infoblox and NAC before running post_SIDA.ps1" -f $OutPut
+
+Write-Host $message
+
+Start-Sleep -Seconds 3
