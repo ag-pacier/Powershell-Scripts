0278cb0023812e6795f8ec54c26b812b47bb4a9e
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2021-06-11 09:43:10 -0400

    Fixed path, added check for netcat

--- SIDA/2_post_SIDA_root.ps1
+++ SIDA/2_post_SIDA_root.ps1
@@ -5,10 +5,24 @@ Gage Pacier's root SIDA script
 #Requires -RunAsAdministrator
 
 Write-Host "Setting connection to PRIVATE"
-Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
+
+$Current = Get-NetConnectionProfile -NetworkCategory
+$CommandRep = 0
+
+while ($Current -ne "Private") {
+    Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
+    Start-Sleep -Seconds 2
+    $CommandRep ++
+    if ($CommandRep -gt 2) {
+        Write-Host "Unable to set the connection to private. Please check the Ethernet connection."
+        Write-Host "Once resolved, please try again."
+        exit
+    }
+}
 
 Write-Host "Starting printer install, please use IP: 10.21.66.253 if in the SIDA Training Room"
-Start-Process -FilePath ".\upd-pcl6-x64-7.0.0.24832.exe"
+Start-Sleep -Seconds 1
+Start-Process -FilePath "upd-pcl6-x64-7.0.0.24832.exe"
 
 Write-Host "Ensure you make a note of what you named the printer for the last step!"
 Write-Host "Enabling Remote Desktop..."
