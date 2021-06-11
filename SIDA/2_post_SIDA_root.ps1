bc1c422d531a908e6ea44cdd65e4fab7b061e3ea
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2021-06-11 09:21:58 -0400

    corrected spelling mistakes

--- SIDA/2_post_SIDA_root.ps1
+++ SIDA/2_post_SIDA_root.ps1
@@ -7,7 +7,7 @@ Gage Pacier's root SIDA script
 Write-Host "Setting connection to PRIVATE"
 Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
 
-Write-Host "Starting printer install, please use IP: 10.11.66.253 if in the SIDA Training Room"
+Write-Host "Starting printer install, please use IP: 10.21.66.253 if in the SIDA Training Room"
 Start-Process -FilePath ".\upd-pcl6-x64-7.0.0.24832.exe"
 
 Write-Host "Ensure you make a note of what you named the printer for the last step!"
@@ -15,5 +15,5 @@ Write-Host "Enabling Remote Desktop..."
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
-Write-Host "Please reboot the PC. Once the training account comes up, please run the final script 3_post_SIDA_train.ps1"
+Write-Host "Please reboot the PC. Once the training account comes up, please run the final script run_3.bat"
 Start-Sleep -Seconds 3
