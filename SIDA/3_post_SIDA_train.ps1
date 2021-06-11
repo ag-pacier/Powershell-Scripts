e5b1b7081cac81aeda1f1fc3baa593feb79b60dd
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2021-06-11 10:07:42 -0400

    Fixed path for reg key

--- SIDA/3_post_SIDA_train.ps1
+++ SIDA/3_post_SIDA_train.ps1
@@ -9,7 +9,7 @@ Write-Host $message
 (Get-WMIObject -ClassName win32_printer | Where-Object -Property Name -eq "$Printername").SetDefaultPrinter()
 
 Write-Host "Turning off Windows default printer selection"
-Set-ItemProperty -Path "HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\Windows" -Name "LegacyDefaultPrinterMode" -Value 1
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows" -Name "LegacyDefaultPrinterMode" -Value 1
 
 Write-Host "All set! Please reach out to IET to double check the work after you have rebooted one more time."
 Start-Sleep -Seconds 3
