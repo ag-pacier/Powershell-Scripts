2d458457cf203a3fc990b092b800c2afcbf8fb9c
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2021-06-11 09:43:27 -0400

    Added messaging and default printer

--- SIDA/3_post_SIDA_train.ps1
+++ SIDA/3_post_SIDA_train.ps1
@@ -4,7 +4,12 @@ SIDA swap script helper
 #>
 
 $Printername = Read-Host -Prompt "Name of the printer you just installed"
-(Get-WMIObject -ClassName win32_printer |Where-Object -Property Name -eq "$Printername").SetDefaultPrinter()
+$message = "Setting {0} as the default printer..." -f $Printername
+Write-Host $message
+(Get-WMIObject -ClassName win32_printer | Where-Object -Property Name -eq "$Printername").SetDefaultPrinter()
+
+Write-Host "Turning off Windows default printer selection"
+Set-ItemProperty -Path "HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\Windows" -Name "LegacyDefaultPrinterMode" -Value 1
 
 Write-Host "All set! Please reach out to IET to double check the work after you have rebooted one more time."
 Start-Sleep -Seconds 3
