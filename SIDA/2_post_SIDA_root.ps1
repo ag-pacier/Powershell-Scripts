c78f9fea4af0841dc790e50dfaf129d8184c1005
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2021-06-11 07:58:03 -0400

    Added space for cleanness
    Also, I was checking to make sure the fDenyTSConnections was valid, it is

--- SIDA/2_post_SIDA_root.ps1
+++ SIDA/2_post_SIDA_root.ps1
@@ -12,7 +12,7 @@ Start-Process -FilePath ".\upd-pcl6-x64-7.0.0.24832.exe"
 
 Write-Host "Ensure you make a note of what you named the printer for the last step!"
 Write-Host "Enabling Remote Desktop..."
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-Name "fDenyTSConnections" -Value 0
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
 Write-Host "Please reboot the PC. Once the training account comes up, please run the final script 3_post_SIDA_train.ps1"
